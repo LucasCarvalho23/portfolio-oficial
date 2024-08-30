6eb9bd9fab47411ba34a1dc27624f385ed48f66f
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-08-30 20:42:36 -0300

    modelos de imagem

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -8,10 +8,15 @@ $(document).ready(() => {
             this.imgPortfolio01 = document.querySelector("#img-portfolio01")
             this.imgPortfolio02 = document.querySelector("#img-portfolio02")
 
-            if (this.width <= 991.98) {
+            if (this.width <= 1024 && this.width >= 500.01) {
                 this.imgPortfolio01.src="../images/mobile/01/btec.png"
                 this.imgPortfolio02.src="../images/mobile/02/escudo.png"
-            } else {
+                console.log('ipad');
+            } if (this.width <= 500) {
+                this.imgPortfolio01.src="../images/mobile/01/btec-mobile.png"
+                this.imgPortfolio02.src="../images/mobile/02/escudo-mobile.png"
+                console.log('mobile');
+            } else if (this.width > 1024.01) {
                 console.log('Desktop');
             }
             

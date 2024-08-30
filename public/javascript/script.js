d7902c104bace0db4c2c5b3a6e306e3c24bd6a7f
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-08-30 20:28:24 -0300

    imagens ajustadas baseadas na resolução

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,6 +1,25 @@
 $(document).ready(() => {
 
-    
+    class Resolution {
+
+        constructor() {
+            this.width = window.screen.width
+            this.height = window.screen.height
+            this.imgPortfolio01 = document.querySelector("#img-portfolio01")
+            this.imgPortfolio02 = document.querySelector("#img-portfolio02")
+
+            if (this.width <= 991.98) {
+                this.imgPortfolio01.src="../images/mobile/01/btec.png"
+                this.imgPortfolio02.src="../images/mobile/02/escudo.png"
+            } else {
+                console.log('Desktop');
+            }
+            
+        }
+
+    }
+
+    const resolution = new Resolution()
 
 })
 

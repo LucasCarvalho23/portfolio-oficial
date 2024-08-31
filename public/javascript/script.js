1f1e3aa16c65fa87999ae94230697ac356b52f0b
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-08-30 21:56:29 -0300

    imagens portfolio ajustadas

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -8,16 +8,17 @@ $(document).ready(() => {
             this.imgPortfolio01 = document.querySelector("#img-portfolio01")
             this.imgPortfolio02 = document.querySelector("#img-portfolio02")
 
-            if (this.width <= 1024 && this.width >= 500.01) {
-                this.imgPortfolio01.src="../images/mobile/01/btec.png"
-                this.imgPortfolio02.src="../images/mobile/02/escudo.png"
-                console.log('ipad');
-            } if (this.width <= 500) {
+            if (this.width <= 500) {
                 this.imgPortfolio01.src="../images/mobile/01/btec-mobile.png"
                 this.imgPortfolio02.src="../images/mobile/02/escudo-mobile.png"
                 console.log('mobile');
+            } if (this.width <= 1024 && this.width >= 500.01) {
+                this.imgPortfolio01.src="../images/mobile/01/btec.png"
+                this.imgPortfolio02.src="../images/mobile/02/escudo.png"
+                console.log('ipad');
             } else if (this.width > 1024.01) {
-                console.log('Desktop');
+                this.imgPortfolio01.src="../images/desktop/01/btec.png"
+                this.imgPortfolio02.src="../images/desktop/02/escudo.png"
             }
             
         }

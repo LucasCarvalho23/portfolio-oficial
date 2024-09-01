4f37e73cc14ad46be7b42826c820ca5e939db33d
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-09-01 14:02:43 -0300

    configuração inicial do layout

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,31 +1,35 @@
 $(document).ready(() => {
 
     class Resolution {
-
         constructor() {
-            this.width = window.screen.width
-            this.height = window.screen.height
-            this.imgPortfolio01 = document.querySelector("#img-portfolio01")
-            this.imgPortfolio02 = document.querySelector("#img-portfolio02")
-
-            if (this.width <= 500) {
-                this.imgPortfolio01.src="../images/mobile/01/btec-mobile.png"
-                this.imgPortfolio02.src="../images/mobile/02/escudo-mobile.png"
-                console.log('mobile');
-            } if (this.width <= 1024 && this.width >= 500.01) {
-                this.imgPortfolio01.src="../images/mobile/01/btec.png"
-                this.imgPortfolio02.src="../images/mobile/02/escudo.png"
-                console.log('ipad');
-            } else if (this.width > 1024.01) {
-                this.imgPortfolio01.src="../images/desktop/01/btec.png"
-                this.imgPortfolio02.src="../images/desktop/02/escudo.png"
-            }
-            
+            this.updateImages();
         }
 
+        updateImages() {
+            this.width = window.innerWidth; // Usa o innerWidth para obter a largura atual da janela
+            this.imgPortfolio01 = document.querySelector("#img-portfolio01");
+            this.imgPortfolio02 = document.querySelector("#img-portfolio02");
+
+            if (this.width <= 500) {
+                this.imgPortfolio01.src = "../images/mobile/01/btec-mobile.png";
+                this.imgPortfolio02.src = "../images/mobile/02/escudo-mobile.png";
+                console.log('mobile');
+            } else if (this.width <= 1024) {
+                this.imgPortfolio01.src = "../images/mobile/01/btec.png";
+                this.imgPortfolio02.src = "../images/mobile/02/escudo.png";
+                console.log('ipad');
+            } else {
+                this.imgPortfolio01.src = "../images/desktop/01/btec.png";
+                this.imgPortfolio02.src = "../images/desktop/02/escudo.png";
+            }
+        }
     }
 
-    const resolution = new Resolution()
+    const resolution = new Resolution();
 
-})
+    // Adiciona o ouvinte de evento para redimensionar a janela
+    $(window).on('resize', () => {
+        resolution.updateImages(); // Recarrega as imagens ao redimensionar a janela
+    });
 
+});
